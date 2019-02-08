ee023e765072a58fbd1f0a79e462b54e44b9acea
mb <mb@M-2.local>
2019-02-08 20:44:46 +0100

    magic button randomness

--- change.js
+++ change.js
@@ -3,8 +3,17 @@ document.getElementsByClassName("navbar-brand");
 var myContentDivs = document.getElementsByClassName("navbar-brand");
 
 
-if (myContentDivs[0].innerHTML === "nuarB") {
+if (myContentDivs[0].innerHTML !== "Braun") {
   myContentDivs[0].innerHTML = "Braun"
 } else {
-  myContentDivs[0].innerHTML = "nuarB"
-} 
+// myContentDivs[0].innerHTML = "nuarB"
+  let string = ['B','r','a','u','n']
+
+
+      item1 = Math.floor(Math.random() * 5)
+      item2 = Math.floor(Math.random() * 5)
+      item3 = Math.floor(Math.random() * 5)
+      item4 = Math.floor(Math.random() * 5)
+      item5 = Math.floor(Math.random() * 5)
+      myContentDivs[0].innerHTML = string[item1]+string[item2]+string[item3]+string[item4]+string[item5]
+}
